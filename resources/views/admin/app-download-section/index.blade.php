d29e3ff49d908c29380ad05aafaaee369e36b270
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2025-10-16 00:09:38 +0600

    App Download Section Error Message Show

--- resources/views/admin/app-download-section/index.blade.php
+++ resources/views/admin/app-download-section/index.blade.php
@@ -25,26 +25,44 @@
                                     <input type="file" name="image" id="image-upload" />
                                     <input type="hidden" name="old_image" value="{{ @$appSection->image }}" />
                                 </div>
+                                @error('image')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
                             </div>
                         </div>
                     </div>
                     <div class="form-group">
-                        <label>Title</label>
-                        <input type="text" name="title" value="{{ @$appSection->title }}" class="form-control">
+                        <label>Title <span class="text-danger">*</span></label>
+                        <input type="text" name="title" value="{{ @$appSection->title ?? old('title') }}"
+                            class="form-control @error('title') is-invalid @enderror">
+                        @error('title')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
                     </div>
                     <div class="form-group">
-                        <label>Description</label>
-                        <textarea name="short_description" class="form-control">{{ @$appSection->short_description }}</textarea>
+                        <label>Description <span class="text-danger">*</span></label>
+                        <textarea name="short_description" class="form-control @error('short_description') is-invalid @enderror">{{ @$appSection->short_description ?? old('short_description') }}</textarea>
+                        @error('short_description')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
                     </div>
                     <div class="form-group">
                         <label>Play Store Link <code>(Leave empty for hide)</code></label>
-                        <input type="text" name="play_store_link" class="form-control"
-                            value="{{ @$appSection->play_store_link }}">
+                        <input type="text" name="play_store_link"
+                            class="form-control @error('play_store_link') is-invalid @enderror"
+                            value="{{ @$appSection->play_store_link ?? old('play_store_link') }}">
+                        @error('play_store_link')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
                     </div>
                     <div class="form-group">
                         <label>Apple Store Link <code>(Leave empty for hide)</code></label>
-                        <input type="text" name="apple_store_link" class="form-control"
-                            value="{{ @$appSection->apple_store_link }}">
+                        <input type="text" name="apple_store_link"
+                            class="form-control @error('apple_store_link') is-invalid @enderror"
+                            value="{{ @$appSection->apple_store_link ?? old('apple_store_link') }}">
+                        @error('apple_store_link')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
                     </div>
                     <button type="submit" class="btn btn-primary">Update</button>
                 </form>
