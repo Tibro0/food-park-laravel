7352cc13ba419f2d1dd34ed8ab27c463978275bc
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2025-10-16 21:39:36 +0600

    Mail Setting Error Message Show

--- resources/views/admin/setting/sections/mail-setting.blade.php
+++ resources/views/admin/setting/sections/mail-setting.blade.php
@@ -7,49 +7,69 @@
                 <div class="row">
                     <div class="col-md-4">
                         <div class="form-group">
-                            <label>Mail Driver</label>
-                            <input name="mail_driver" type="text" class="form-control"
+                            <label>Mail Driver <span class="text-danger">*</span></label>
+                            <input name="mail_driver" type="text"
+                                class="form-control @error('mail_driver') is-invalid @enderror"
                                 value="{{ config('settings.mail_driver') }}">
+                            @error('mail_driver')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="form-group">
-                            <label>Mail Host</label>
-                            <input name="mail_host" type="text" class="form-control"
+                            <label>Mail Host <span class="text-danger">*</span></label>
+                            <input name="mail_host" type="text"
+                                class="form-control @error('mail_host') is-invalid @enderror"
                                 value="{{ config('settings.mail_host') }}">
+                            @error('mail_host')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="form-group">
-                            <label>Mail Port</label>
-                            <input name="mail_port" type="text" class="form-control"
+                            <label>Mail Port <span class="text-danger">*</span></label>
+                            <input name="mail_port" type="text"
+                                class="form-control @error('mail_port') is-invalid @enderror"
                                 value="{{ config('settings.mail_port') }}">
+                            @error('mail_port')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
                         </div>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-md-6">
                         <div class="form-group">
-                            <label>Mail Username</label>
-                            <input name="mail_username" type="text" class="form-control"
+                            <label>Mail Username <span class="text-danger">*</span></label>
+                            <input name="mail_username" type="text"
+                                class="form-control @error('mail_username') is-invalid @enderror"
                                 value="{{ config('settings.mail_username') }}">
+                            @error('mail_username')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
                         </div>
                     </div>
                     <div class="col-md-6">
                         <div class="form-group">
-                            <label>Mail Password</label>
-                            <input name="mail_password" type="text" class="form-control"
+                            <label>Mail Password <span class="text-danger">*</span></label>
+                            <input name="mail_password" type="text"
+                                class="form-control @error('mail_password') is-invalid @enderror"
                                 value="{{ config('settings.mail_password') }}">
+                            @error('mail_password')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
                         </div>
                     </div>
                 </div>
                 <div class="form-group">
-                    <label>Mail Form Address</label>
+                    <label>Mail Form Address <span class="text-danger">*</span></label>
                     <input name="mail_from_address" type="text" class="form-control"
                         value="{{ config('settings.mail_from_address') }}">
                 </div>
                 <div class="form-group">
-                    <label>Mail Receive Address</label>
+                    <label>Mail Receive Address <span class="text-danger">*</span></label>
                     <input name="mail_receive_address" type="text" class="form-control"
                         value="{{ config('settings.mail_receive_address') }}">
                 </div>
