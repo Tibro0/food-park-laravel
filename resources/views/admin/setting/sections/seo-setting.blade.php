d6507dde9ef8a4cc0d8c229c646bc2e1562adaa2
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2025-10-16 21:43:57 +0600

    SEO Setting Error Message Show

--- resources/views/admin/setting/sections/seo-setting.blade.php
+++ resources/views/admin/setting/sections/seo-setting.blade.php
@@ -9,18 +9,28 @@
                 @csrf
                 @method('PUT')
                 <div class="form-group">
-                    <label for="">Seo Title</label>
-                    <input name="seo_title" type="text" class="form-control"
+                    <label>Seo Title <span class="text-danger">*</span></label>
+                    <input name="seo_title" type="text" class="form-control @error('seo_title') is-invalid @enderror"
                         value="{{ config('settings.seo_title') }}">
+                    @error('seo_title')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
                 </div>
                 <div class="form-group">
-                    <label for="">Seo Description</label>
-                    <textarea name="seo_description" class="form-control">{{ config('settings.seo_description') }}</textarea>
+                    <label>Seo Description <span class="text-danger">*</span></label>
+                    <textarea name="seo_description" class="form-control @error('seo_description') is-invalid @enderror">{{ config('settings.seo_description') }}</textarea>
+                    @error('seo_description')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
                 </div>
                 <div class="form-group">
                     <label>Seo Keywords</label>
-                    <input type="text" name="seo_keywords" class="form-control inputtags"
+                    <input type="text" name="seo_keywords"
+                        class="form-control inputtags @error('seo_keywords') is-invalid @enderror"
                         value="{{ config('settings.seo_keywords') }}">
+                    @error('seo_keywords')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
                 </div>
                 <button type="submit" class="btn btn-primary">Save</button>
             </form>
