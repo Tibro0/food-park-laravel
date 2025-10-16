022687ef210153e70366fa3edbda5641cc05e737
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2025-10-16 18:52:25 +0600

    Footer Info Error Message Show

--- resources/views/admin/footer-info/index.blade.php
+++ resources/views/admin/footer-info/index.blade.php
@@ -20,23 +20,42 @@
                     @method('PUT')
                     <div class="form-group">
                         <label>Short Info</label>
-                        <textarea name="short_info" class="form-control">{{ @$footerInfo->short_info }}</textarea>
+                        <textarea name="short_info" class="form-control @error('short_info') is-invalid @enderror">{{ @$footerInfo->short_info }}</textarea>
+                        @error('short_info')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
                     </div>
                     <div class="form-group">
                         <label>Address</label>
-                        <input type="text" name="address" value="{{ @$footerInfo->address }}" class="form-control">
+                        <input type="text" name="address" value="{{ @$footerInfo->address ?? old('address') }}"
+                            class="form-control @error('address') is-invalid @enderror">
+                        @error('address')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
                     </div>
                     <div class="form-group">
                         <label>Phone</label>
-                        <input type="text" name="phone" value="{{ @$footerInfo->phone }}" class="form-control">
+                        <input type="text" name="phone" value="{{ @$footerInfo->phone ?? old('phone') }}"
+                            class="form-control @error('phone') is-invalid @enderror">
+                        @error('phone')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
                     </div>
                     <div class="form-group">
                         <label>Email</label>
-                        <input type="text" name="email" value="{{ @$footerInfo->email }}" class="form-control">
+                        <input type="text" name="email" value="{{ @$footerInfo->email ?? old('email') }}"
+                            class="form-control @error('email') is-invalid @enderror">
+                        @error('email')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
                     </div>
                     <div class="form-group">
                         <label>Copyright</label>
-                        <input type="text" name="copyright" value="{{ @$footerInfo->copyright }}" class="form-control">
+                        <input type="text" name="copyright" value="{{ @$footerInfo->copyright ?? old('copyright') }}"
+                            class="form-control @error('copyright') is-invalid @enderror">
+                        @error('copyright')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
                     </div>
                     <button type="submit" class="btn btn-primary">Update</button>
                 </form>
