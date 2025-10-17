301b1084197560043710c28d5ecb3508f30df087
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2025-10-17 20:26:52 +0600

    User Seeder Information Update

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
@@ -19,15 +20,18 @@ class UserSeeder extends Seeder
                 'name' => 'Admin',
                 'email' => 'admin@gmail.com',
                 'role' => 'admin',
-                'password' => Hash::make('password')
+                'email_verified_at' => now(),
+                'password' => Hash::make('password'),
+                'remember_token' => Str::random(10),
             ],
             [
                 'name' => 'User',
                 'email' => 'user@gmail.com',
                 'role' => 'user',
-                'password' => Hash::make('password')
+                'email_verified_at' => now(),
+                'password' => Hash::make('password'),
+                'remember_token' => Str::random(10),
             ],
         ]);
     }
-
 }
