32f4370153232804208f18d67007a8142ba104e2
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2026-01-15 01:23:08 +0600

    ReadMe File Update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -72,13 +72,13 @@
                                     </div>
                                 </div>
                             </form>
-                            {{-- <p class="or"><span>or</span></p> --}}
-                            {{-- <ul class="d-flex">
+                            <p class="or"><span>or</span></p>
+                            <ul class="d-flex">
                                 <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
                                 <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
                                 <li><a href="#"><i class="fab fa-twitter"></i></a></li>
                                 <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
-                            </ul> --}}
+                            </ul>
                             <p class="create_account">Dont’t have an aceount ? <a href="{{ route('register') }}">Create
                                     Account</a>
                             </p>
