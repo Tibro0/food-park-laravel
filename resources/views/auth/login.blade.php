5ef587b6b7519fdf83af493516dc2db3563ab6a8
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2026-01-26 15:27:56 +0600

    Icon Update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -74,10 +74,8 @@
                             </form>
                             <p class="or"><span>or</span></p>
                             <ul class="d-flex">
-                                <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                                <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
-                                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                                <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                                <li><a href="#"><i class="fab fa-google"></i></a></li>
+                                <li><a href="#"><i class="fab fa-github"></i></a></li>
                             </ul>
                             <p class="create_account">Dont’t have an aceount ? <a href="{{ route('register') }}">Create
                                     Account</a>
