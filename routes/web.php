c309750adfac3be98ffc7239cebb94815403fe1b
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2026-01-26 19:34:33 +0600

    Web Routs Update

--- routes/web.php
+++ routes/web.php
@@ -13,56 +13,75 @@ use App\Http\Controllers\Frontend\ProfileController;
 use App\Http\Controllers\Frontend\WishlistController;
 use Illuminate\Support\Facades\Route;
 
+//////////////////////////////////
+Route::get('tibro', function () {
+    // return session()->flush();
+    return session()->all();
+});
+/////////////////////////////////
+
 /** Admin Auth Routes */
 Route::group(['middleware' => 'guest'], function () {
-    Route::get('admin/login', [AdminAuthController::class, 'index'])->name('admin.login');
-    Route::get('admin/forget-password', [AdminAuthController::class, 'forgetPassword'])->name('admin.forget-password');
+    Route::controller(AdminAuthController::class)->group(function () {
+        Route::get('admin/login', 'index')->name('admin.login');
+        Route::get('admin/forget-password', 'forgetPassword')->name('admin.forget-password');
+    });
 });
 
-Route::group(['middleware' => 'auth'], function(){
+Route::group(['middleware' => 'auth'], function () {
     /** User Dashboard Route */
-    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
-    Route::get('user-dashboard-list-style', [DashboardController::class, 'userDashboardListStyle'])->name('user-dashboard-list-style');
-    
+    Route::controller(DashboardController::class)->group(function () {
+        Route::get('dashboard', 'index')->name('dashboard');
+        Route::get('user-dashboard-list-style', 'userDashboardListStyle')->name('user-dashboard-list-style');
+    });
+
     /** User Profile Update Route */
-    Route::put('profile', [ProfileController::class, 'updateProfile'])->name('profile.update');
-    Route::put('profile/password', [ProfileController::class, 'updatePassword'])->name('profile.password.update');
-    Route::post('profile/avatar', [ProfileController::class, 'updateAvatar'])->name('profile.avatar.update');
+    Route::controller(ProfileController::class)->group(function () {
+        Route::put('profile', 'updateProfile')->name('profile.update');
+        Route::put('profile/password', 'updatePassword')->name('profile.password.update');
+        Route::post('profile/avatar', 'updateAvatar')->name('profile.avatar.update');
+    });
 
     /** User Address Route */
-    Route::post('address', [AddressController::class, 'createAddress'])->name('address.store');
-    Route::put('address/{id}/edit', [AddressController::class, 'updateAddress'])->name('address.update');
-    Route::delete('address/{id}', [AddressController::class, 'destroyAddress'])->name('address.destroy');
+    Route::controller(AddressController::class)->group(function () {
+        Route::post('address', 'createAddress')->name('address.store');
+        Route::put('address/{id}/edit', 'updateAddress')->name('address.update');
+        Route::delete('address/{id}', 'destroyAddress')->name('address.destroy');
+    });
 
-    /** checkout Route */
-    Route::get('checkout', [CheckoutController::class, 'index'])->name('checkout.index');
-    Route::get('checkout/{id}/delivery-cal', [CheckoutController::class, 'CalculateDeliveryCharge'])->name('checkout.delivery-cal');
-    Route::post('checkout', [CheckoutController::class, 'checkoutRedirect'])->name('checkout.redirect');
+    /** Checkout Route */
+    Route::controller(CheckoutController::class)->group(function () {
+        Route::get('checkout', 'index')->name('checkout.index');
+        Route::get('checkout/{id}/delivery-cal', 'CalculateDeliveryCharge')->name('checkout.delivery-cal');
+        Route::post('checkout', 'checkoutRedirect')->name('checkout.redirect');
+    });
 
     /** Payment Routes */
-    Route::get('payment', [PaymentController::class, 'index'])->name('payment.index');
-    Route::post('make-payment', [PaymentController::class, 'makePayment'])->name('make-payment');
+    Route::controller(PaymentController::class)->group(function () {
+        Route::get('payment', 'index')->name('payment.index');
+        Route::post('make-payment', 'makePayment')->name('make-payment');
 
-    Route::get('payment-success', [PaymentController::class, 'paymentSuccess'])->name('payment.success');
-    Route::get('payment-cancel', [PaymentController::class, 'paymentCancel'])->name('payment.cancel');
+        Route::get('payment-success', 'paymentSuccess')->name('payment.success');
+        Route::get('payment-cancel', 'paymentCancel')->name('payment.cancel');
 
-    /** PayPal Routes */
-    Route::get('paypal/payment', [PaymentController::class, 'payWithPaypal'])->name('paypal.payment');
-    Route::get('paypal/success', [PaymentController::class, 'paypalSuccess'])->name('paypal.success');
-    Route::get('paypal/cancel', [PaymentController::class, 'paypalCancel'])->name('paypal.cancel');
+        /** PayPal Routes */
+        Route::get('paypal/payment', 'payWithPaypal')->name('paypal.payment');
+        Route::get('paypal/success', 'paypalSuccess')->name('paypal.success');
+        Route::get('paypal/cancel', 'paypalCancel')->name('paypal.cancel');
 
-    /** Stripe Routes */
-    Route::get('stripe/payment', [PaymentController::class, 'payWithStripe'])->name('stripe.payment');
-    Route::get('stripe/success', [PaymentController::class, 'stripeSuccess'])->name('stripe.success');
-    Route::get('stripe/cancel', [PaymentController::class, 'stripeCancel'])->name('stripe.cancel');
+        /** Stripe Routes */
+        Route::get('stripe/payment', 'payWithStripe')->name('stripe.payment');
+        Route::get('stripe/success', 'stripeSuccess')->name('stripe.success');
+        Route::get('stripe/cancel', 'stripeCancel')->name('stripe.cancel');
 
-    /** Razorpay Routes */
-    Route::get('razorpay-redirect', [PaymentController::class, 'razorpayRedirect'])->name('razorpay-redirect');
-    Route::post('razorpay/payment', [PaymentController::class, 'payWithRazorpay'])->name('razorpay.payment');
+        /** Razorpay Routes */
+        Route::get('razorpay-redirect', 'razorpayRedirect')->name('razorpay-redirect');
+        Route::post('razorpay/payment', 'payWithRazorpay')->name('razorpay.payment');
+    });
 });
 
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
 
 
 /** Home Route */
@@ -127,8 +146,3 @@ Route::get('cart-destroy', [CartController::class, 'cartDestroy'])->name('cart.d
 /** Coupon Routes */
 Route::post('apply-coupon', [FrontendController::class, 'applyCoupon'])->name('apply-coupon');
 Route::get('destroy-coupon', [FrontendController::class, 'destroyCoupon'])->name('destroy-coupon');
-
-Route::get('tibro', function () {
-    // return session()->flush();
-    return session()->all();
-});
