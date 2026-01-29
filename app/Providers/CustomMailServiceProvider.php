31a1515ca3b0a34cef78288877d2da7cb8644673
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2026-01-29 23:15:11 +0600

    GitHub Login Set

--- app/Providers/CustomMailServiceProvider.php
+++ app/Providers/CustomMailServiceProvider.php
@@ -22,8 +22,8 @@ class CustomMailServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $mailSetting = Cache::rememberForever('mail_settings', function(){
-            $key = ['mail_driver', 'mail_host', 'mail_port', 'mail_username', 'mail_password', 'mail_from_address', 'mail_receive_address'];
+        $mailSetting = Cache::rememberForever('mail_settings', function () {
+            $key = ['mail_driver', 'mail_host', 'mail_port', 'mail_username', 'mail_password', 'mail_from_address', 'mail_receive_address', 'github_client_id', 'github_client_secret', 'github_redirect_url', 'google_client_id', 'google_client_secret', 'google_redirect_url'];
 
             return Setting::whereIn('key', $key)->pluck('value', 'key')->toArray();
         });
@@ -34,6 +34,10 @@ class CustomMailServiceProvider extends ServiceProvider
             Config::set('mail.mailers.smtp.username', $mailSetting['mail_username']);
             Config::set('mail.mailers.smtp.password', $mailSetting['mail_password']);
             Config::set('mail.from.address', $mailSetting['mail_from_address']);
+
+            Config::set('services.github.client_id', $mailSetting['github_client_id']);
+            Config::set('services.github.client_secret', $mailSetting['github_client_secret']);
+            Config::set('services.github.redirect', $mailSetting['github_redirect_url']);
         }
     }
 }
