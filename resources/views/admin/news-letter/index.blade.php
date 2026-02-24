3e6e5e8deb6eebfcab3b60f9e8ce41b12d8edabf
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2026-02-24 15:10:28 +0600

    News Letter Update

--- resources/views/admin/news-letter/index.blade.php
+++ resources/views/admin/news-letter/index.blade.php
@@ -13,53 +13,55 @@
         <div class="section-header">
             <h1>Subscribers</h1>
         </div>
-
-        <div class="card">
-            <div class="card-body">
-                <div id="accordion">
-                    <div class="accordion">
-                        <div class="accordion-header collapsed bg-primary text-light p-3 " role="button"
-                            data-toggle="collapse" data-target="#panel-body-1" aria-expanded="false">
-                            <h4>Send News Letter..</h4>
-                        </div>
-                        <div class="accordion-body collapse" id="panel-body-1" data-parent="#accordion" style="">
-                            <form action="{{ route('admin.news-letter.send') }}" method="POST">
-                                @csrf
-                                <div class="form-group">
-                                    <label>Subject <span class="text-danger">*</span></label>
-                                    <input type="text" class="form-control @error('subject') is-invalid @enderror"
-                                        name="subject" value="{{ old('subject') }}">
-                                    @error('subject')
-                                        <div class="invalid-feedback">{{ $message }}</div>
-                                    @enderror
-                                </div>
-                                <div class="form-group">
-                                    <label>Message <span class="text-danger">*</span></label>
-                                    <textarea name="message" class="summernote form-control @error('message') is-invalid @enderror">{{ old('message') }}</textarea>
-                                    @error('message')
-                                        <div class="invalid-feedback">{{ $message }}</div>
-                                    @enderror
-                                </div>
-                                <button type="submit" class="btn btn-primary">Send</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-
-
-    <section class="section">
         <div class="card card-primary">
             <div class="card-header">
                 <h4>All Subscribers</h4>
+                <div class="card-header-action">
+                    <button class="btn btn-outline-primary" data-toggle="modal" data-target="#exampleModal">Send News
+                        Letter</button>
+                </div>
             </div>
             <div class="card-body">
                 {{ $dataTable->table() }}
             </div>
         </div>
     </section>
+@endsection
+
+@section('modal')
+    <div class="modal fade" tabindex="-1" role="dialog" id="exampleModal">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Send News Letter..</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form action="{{ route('admin.news-letter.send') }}" method="POST">
+                        @csrf
+                        <div class="form-group">
+                            <label>Subject <span class="text-danger">*</span></label>
+                            <input type="text" class="form-control @error('subject') is-invalid @enderror" name="subject"
+                                value="{{ old('subject') }}">
+                            @error('subject')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <label>Message <span class="text-danger">*</span></label>
+                            <textarea name="message" class="summernote form-control @error('message') is-invalid @enderror">{{ old('message') }}</textarea>
+                            @error('message')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <button type="submit" class="btn btn-primary">Send</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 
 @push('admin-js')
