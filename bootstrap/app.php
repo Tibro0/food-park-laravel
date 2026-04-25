003ed309afccc3a104528267bc50d119a49e786f
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2026-04-25 22:22:41 +0600

    Laravel Sanctum User Login & Logout

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\ApiRoleMiddleware;
 use App\Http\Middleware\RoleMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -7,11 +8,11 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
-        then: function(){
+        then: function () {
             Route::middleware(['web', 'auth', 'role:admin'])
                 ->prefix('admin')
                 ->as('admin.')
@@ -21,6 +22,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'role' => RoleMiddleware::class,
+            'apiRole' => ApiRoleMiddleware::class
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,25 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+});
+
+Route::group(['middleware' => ['auth:sanctum', 'apiRole:admin'], 'prefix' => 'admin'], function () {
+    Route::controller(AuthController::class)->group(function () {
+        Route::post('logout', 'logout');
+    });
+});
+
+Route::group(['middleware' => ['auth:sanctum', 'apiRole:user'], 'prefix' => 'user'], function () {
+    Route::controller(AuthController::class)->group(function () {
+        Route::post('logout', 'logout');
+    });
+});
