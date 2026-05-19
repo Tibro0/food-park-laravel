a11ab06b18b770f219b378ebebe07e5c95b163ba
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2026-05-19 23:24:59 +0600

    Error Page Add

--- resources/views/errors/419.blade.php
+++ resources/views/errors/419.blade.php
@@ -1,7 +1,7 @@
 @extends('frontend.layouts.master')
 
 @push('frontend-css')
-    <title>FoodPark || Payment Successful</title>
+    <title>FoodPark || 419 Page</title>
 @endpush
 
 @section('frontend-content')
@@ -10,10 +10,10 @@
         <div class="fp__breadcrumb_overlay">
             <div class="container">
                 <div class="fp__breadcrumb_text">
-                    <h1>Payment Successful</h1>
+                    <h1>419 Page</h1>
                     <ul>
                         <li><a href="{{ route('home') }}">home</a></li>
-                        <li><a href="javascript:;">Payment Successful</a></li>
+                        <li><a href="javascript:;">419 Page</a></li>
                     </ul>
                 </div>
             </div>
@@ -21,26 +21,17 @@
     </section>
     <!--=============================BREADCRUMB END==============================-->
 
-
-    <!--============================PAYMENT PAGE START==============================-->
+    <!--============================419 PAGE START==============================-->
     <section class="fp__payment_page mt_100 xs_mt_70 mb_100 xs_mb_70">
         <div class="container">
             <div class="row">
-                <div class="text-center">
-                    <div class="mb-4">
-                        <i class="fas fa-check"
-                            style="font-size: 70px;
-                        background: green;
-                        padding: 20px;
-                        border-radius: 50%;
-                        color: #fff;"></i>
-                    </div>
-                    <h4>Order Placed Successfully!</h4>
-                    <a class="common_btn mt-4" href="{{ route('dashboard') }}">Go to Dahsboard</a>
+                <div class="col-12 text-center">
+                    <h2>419 - Page Expired</h2>
+                    <p>The page you are looking for has expired.</p>
+                    <a class="common_btn mt-4" href="{{ route('dashboard') }}">Go to Home</a>
                 </div>
-
             </div>
         </div>
     </section>
-    <!--============================PAYMENT PAGE END==============================-->
+    <!--============================419 PAGE END==============================-->
 @endsection
